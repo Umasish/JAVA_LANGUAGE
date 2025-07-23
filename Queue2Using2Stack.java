5cdb6929e4f94d6d80c47a55c526999919e707a7
Umasish Patra <umasishpatra2002@gmail.com>
2025-07-23 13:06:58 +0530

    Stack last and queue first part

---
+++ Queue2Using2Stack.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class Queue2Using2Stack {
+    static class Queue{
+        static Stack<Integer> s1 = new Stack<>();
+        static Stack<Integer> s2 = new Stack<>();
+        static boolean isEmpty(){
+            return s1.isEmpty();
+        }
+        static void add(int data){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            s1.push(data);
+            while(!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+        }
+        static int remove(){
+            if(isEmpty()){
+                System.out.println("queue is empty");
+                return -1;
+            }
+            return s1.pop();
+        }
+        static int peek(){
+            if(isEmpty()){
+                System.out.println("queue is empty");
+                return -1;
+            }
+            return s1.peek();
+        }
+    }
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
