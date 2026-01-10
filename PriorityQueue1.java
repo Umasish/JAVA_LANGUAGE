32fed9515a6a9fd2598dd8273373e1cd3de567b3
Umasish Patra <umasishpatra2002@gmail.com>
2026-01-10 19:03:18 +0530

    priority queue

---
+++ PriorityQueue1.java
@@ -0,0 +1,29 @@
+import java.util.PriorityQueue;
+public class PriorityQueue1 {
+    static class Student implements Comparable<Student> {
+        String name;
+        int rank;
+
+        Student(String name , int rank){
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student s2){
+            return this.rank - s2.rank;
+        }
+    }
+    public static void main(String[] args) {
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+        pq.add(new Student("a", 4));
+        pq.add(new Student("b", 3));
+        pq.add(new Student("c", 2));
+
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek().name + " ->" + pq.peek().rank);
+            pq.remove();
+
+        }
+    }
+}
